4f13dccfe9acb7a9d26521d3dc7d38f758d30131
rodrigosevero <rodrigoseverodev@gmail.com>
2022-03-05 22:40:24 -0400

    Commit Inicial

---
+++ README.md
@@ -0,0 +1,21 @@
+cp .env.example .env
+
+docker-compose up
+
+Abrir outra instancia do terminal e rodar os seguintes comandos:
+
+docker compose exec app composer install
+
+docker compose exec app php artisan key:generate
+
+docker compose exec app php artisan storage:link
+
+docker compose exec app php artisan migrate
+
+docker compose exec app php artisan db:seed
+
+---------------------------------------------------
+Dados de acesso:
+http://localhost:8000/
+Login: admin@admin.com
+senha: 123456
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'created_at' => date("Y-m-d h:i:s"),
+            'password' => bcrypt('123456'),            
+        ]);
+        
+    }
+}
